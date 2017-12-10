3e898d725cc5711482e9275c172f2b3ab8432d74
Morten Laukvik <morten@laukvik.no>
2017-12-10 11:17:02 +0100

    Express: added slash

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+var express = require("express");
+// const app = express();
+// const path = require('path');
 
-app.use(express.static('./dist'));
+// app.use(express.static('./dist'));
 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/index.html'));
 // });
 
 const port = process.env.PORT || 8080;
-
+var app = express();
+app.use("/", express.static('dist'));
 app.listen(port, () => console.log('Smartflow-Web listening at ' + port + " "));
