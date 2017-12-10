2dcd71e1edc1b879b2fd269c9d22cffb66e06a39
Morten Laukvik <morten@laukvik.no>
2017-12-10 11:29:52 +0100

    Express: using dist

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require("express");
-var application_root = __dirname;
-
-const port = process.env.PORT || 8080;
-var app = express();
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 80;
+const app = express();
+const application_root = path.join(__dirname, 'dist');
 app.use(express.static(application_root));
 app.listen(port, () => console.log('Smartflow-Web listening on port %d in %s mode from ', port, app.settings.env, application_root));
