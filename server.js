fd0bd019cd31127c7ff63b28e5a3eb0c263cca99
Morten Laukvik <morten@laukvik.no>
2017-12-10 11:48:11 +0100

    Express: using route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 const app = express();
 const application_root = path.join(__dirname, 'dist');
-app.use(express.static(application_root));
+// app.use(express.static("dist"));
+app.use('/', express.static(application_root));
 app.listen(port, () => console.log('Smartflow-Web listening on port %d in %s mode from ', port, app.settings.env, application_root));
