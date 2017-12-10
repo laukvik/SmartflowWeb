70e317fb6b722571726db987cc05260bf19cfd62
Morten Laukvik <morten@laukvik.no>
2017-12-10 10:40:22 +0100

    Added webserver

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log('Smartflow-Web app listening at ' + port));
