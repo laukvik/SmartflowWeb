f63c8cbd1bb0c6631723d4e4d676bdc9367a1ec9
Morten Laukvik <morten@laukvik.no>
2017-12-10 11:25:07 +0100

    Express: Trying without dist

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 var express = require("express");
-// const app = express();
-// const path = require('path');
-
-// app.use(express.static('./dist'));
-
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'));
-// });
+var application_root = __dirname;
 
 const port = process.env.PORT || 8080;
 var app = express();
-app.use("/", express.static('dist'));
-app.listen(port, () => console.log('Smartflow-Web listening at ' + port + " "));
+app.use(express.static(application_root));
+app.listen(port, () => console.log('Smartflow-Web listening on port %d in %s mode from ', port, app.settings.env, application_root));
